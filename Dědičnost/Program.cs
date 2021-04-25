de0beb4701c1cd07b0349a610e183af34d0afcc3
Tomexe <t_dano@utb.cz>
2021-04-25 20:59:02 +0200

    Add project files.

---
+++ Dědičnost/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Test1
+{
+    class Osoba
+    {
+        public string Jmeno { get; set; }
+        
+    }
+    // Do následujícího kódu doplňte definici třídy Zamestnanec a napište jaké jsou výhody a nevýhody dědičnosti kódu.
+
+    class Zamestnanec : Osoba
+    {
+        public double Mzda { get; set; }
+
+        public Zamestnanec(string jmeno, double mzda)
+        {
+            Jmeno = jmeno;
+            Mzda = mzda;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jmeno = "Jirina";
+            double mzda = 35000;
+            Zamestnanec zamestnanec = new Zamestnanec(jmeno, mzda);
+            Console.WriteLine($"Zamestnanec jmeno: {zamestnanec.Jmeno} mzda: {zamestnanec.Mzda}");
+        }
+    }
+}
